554f533a276a68518044177f80a0d4b70118f159
youjxie <youjxie@tencent.com>
2018-12-07 14:48:15 +0800

    分文件优化

---
+++ codec/ctx/CtxProtoCodec.h
@@ -0,0 +1,50 @@
+#ifndef _TITANS_CODEC_CTX_CTXPROTOCODEC_H_
+#define _TITANS_CODEC_CTX_CTXPROTOCODEC_H_
+
+#include <core/codec/BaseCodec.h>
+
+namespace TITANS {
+
+namespace CODEC {
+
+template<typename REQ, typename RSP>
+class CtxProtoCodec : public BaseCodec {
+
+public:
+    CtxProtoCodec() {}
+
+    virtual ~CtxProtoCodec() {}
+
+    virtual int RspPkgEncode(std::string& strData) {
+        if (!_bodyReq.SerializeToString(&strData)) {
+            return 4; //ILIVE_EC_INNER_ERROR
+        }
+        return 0;
+    }
+
+    virtual int ReqPkgDecode(const char* pData, const unsigned int len, void* pExt) {
+        
+        if (!_bodyRsp.ParseFromArray(pData, len)) {
+            return 1; //ILIVE_EC_REQ_BAD_PKG
+        }
+        return 0;
+    }
+
+    REQ& BodyReq() {
+        return _bodyReq;
+    }
+
+    RSP& BodyRsp() {
+        return _bodyRsp;
+    }
+
+protected:
+    REQ _bodyReq;
+    RSP _bodyRsp;
+};
+
+} //namespace CODEC
+
+} //namespace TITANS
+
+#endif //_TITANS_CODEC_CTX_CTXPROTOCODEC_H_
